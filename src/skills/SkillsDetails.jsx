c04edbf724309c7dcc2066f47ca82d1627cafb45
John Nabers <jmn17sw@gmail.com>
2025-07-09 11:41:38 -0500

    created skills page and corresponding details

---
+++ src/skills/SkillsDetails.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router";
+import useQuery from "../api/useQuery";
+
+export default function SkillDetails() {
+const { id } = useParams();
+const {
+  data: skill,
+  loading,
+  error,
+} = useQuery(`/skills/${id}`, "skill");
+
+if (loading) return <p>Loading...</p>;
+if(error || !skill) return <p>Sorry! {error}</p>;
+
+return (
+  <>
+    <h1>{skill.name}</h1>
+    <p>{skill.magicPoints}</p>
+    <p>{skill.damage}</p>
+  </>
+  );
+}
+
---
+++ src/skills/SkillsPage.jsx
@@ -0,0 +1,11 @@
+import SkillList from "./SkillList"
+
+export default function SkillsPage() {
+
+  return (
+    <>
+      <h1>Skills</h1>
+      <SkillList />
+    </>
+  );
+}
