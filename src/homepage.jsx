c49e3fbf07c305eadbe0a744a6dfd50f7769e41f
Matthew Bailey <Matthewbailey916@gmail.com>
2025-07-10 13:25:54 -0500

    created BackgroundMedia.jsx and added styling to CSS for NAV BAR.  Background MP4 added & styled.  BG MP3 added (error, needs ticket for resolution) - additinal changes were made to Layout.jsx, Navbar.jsx, Homepage.jsx and main.jsx.  Media folder was moved to PUBLIC for accessibility

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,20 +1,14 @@
 import { useNavigate } from 'react-router';
 
-
 export default function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <>
-
-      <div className="content">
-        <h1>Welcome Adventurers!!</h1>
-        <p>Begin Your Quest...</p>
-
-        <button onClick={() => navigate('/sessions')} className="btn">
+         <div className="content">
+        <h1>Welcome Adventurers!</h1>
+               <button onClick={() => navigate('/sessions')} className="btn">
           View Sessions
         </button>
       </div>
-    </>
-  );
+      );
 }
