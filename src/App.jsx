182945285383b037b685f3a7c8e366e744c2c0cb
John Nabers <jmn17sw@gmail.com>
2025-07-10 11:37:40 -0500

    added skills list route tp app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { OneCharacter } from "./characters/OneCharacter.jsx";
 import { NewCharacter } from "./characters/NewCharacter.jsx";
 import Sessions from "./sessions/sessions";
 import SessionById from "./sessions/SessionById.jsx";
+import { SkillsList } from "./skills/skillsList.jsx"
 
 
 
@@ -25,6 +26,7 @@ export default function App() {
           <Route path="/characters" element={<AllCharacters />}/>
           <Route path="/characters/:id" element={<OneCharacter />}/>
           <Route path="/characters/new" element={<NewCharacter />}/>
+          <Route path="/skills/list" element={<SkillsList />}/>
           <Route path="*" element={<p>Page not found</p>} />
         </Route>
       </Routes>
