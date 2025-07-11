ae7473134c487164b747473266cecea4697fdbcb
Matthew Bailey <Matthewbailey916@gmail.com>
2025-07-11 11:26:21 -0500

    corrected error on homepage.jsx + css edit to z-index's (not working right yet)

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -11,23 +11,16 @@ export default function HomePage() {
     "/media/RogueImg.png",
     "/media/WarriorImg.png",
   ];
-
-  mediaCss
+  
   return (
     <div className="content">
-
       <div className="page-box">
-        <h1>Welcome Adventurers</h1>
+        <h1>Welcome Adventurers!!</h1>
+        <p>Begin Your Quest...</p>
         <button onClick={() => navigate('/sessions')} className="btn">
           View Sessions
         </button>
-
-      <div className="content">
-        <h1>Welcome Adventurers!!</h1>
-        <p>Begin Your Quest...</p>
-main
       </div>
-
       <div className="page-box">
         <ImageCarousel images={slides} interval={3000} />
       </div>
